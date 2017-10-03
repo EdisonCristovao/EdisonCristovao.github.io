4f2e18eea58600951ba2495947c6dd8e3a1c75c3
Junior Edison <edison.cr.jr@gmail.com>
2017-10-03 19:27:13 -0300

    Algumas novas alteraçoes

--- js/style.js
+++ js/style.js
@@ -1,2 +1,12 @@
-$(".button-collapse").sideNav();
-$('#id-under').addClass('animated flash infinite');
+
+
+$(document).ready(function() {
+    $(".button-collapse").sideNav();
+	$('#id-under').addClass('animated flash infinite');
+
+    $(document).scroll(function(event) {
+    	if ($(this).scrollTop()>=10) {
+			$('h3').addClass('animated bounce');
+		}
+     });
+});
