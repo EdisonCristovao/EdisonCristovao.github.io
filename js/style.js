cb9baf0a0027b4010b725a03a7d961d1636cf6d6
Junior Edison <edison.cr.jr@gmail.com>
2017-10-04 00:52:11 -0300

    ancoras

--- js/style.js
+++ js/style.js
@@ -1,8 +1,44 @@
 
 
-$(document).ready(function() {
+$(document).ready(function($) {
     $(".button-collapse").sideNav();
 	$('#id-under').addClass('animated flash infinite');
+
+	$('.inicio-anc').click(function(event) {
+		$('.button-collapse').sideNav('hide');
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+	});
+
+	$('.sobre-anc').click(function(event) {
+		$('.button-collapse').sideNav('hide');
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+	});
+
+	$('.objetivo-anc').click(function(event) {
+		$('.button-collapse').sideNav('hide');
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+	});
+
+	$('.escolaridade-anc').click(function(event) {
+		$('.button-collapse').sideNav('hide');
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+	});
+
+	$('.profiss-anc').click(function(event) {
+		$('.button-collapse').sideNav('hide');
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+	});
+
+	$('.cursos-anc').click(function(event) {
+		$('.button-collapse').sideNav('hide');
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+	});
 
     $(document).scroll(function(event) {
     	if ($(this).scrollTop()>=10) {
